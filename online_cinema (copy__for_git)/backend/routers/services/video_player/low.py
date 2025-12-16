7e14078b78a0e3d28701046ea16cb441e7692106
DenysRybak <119297864+DenysRybak@users.noreply.github.com>
2025-12-16 15:20:35 +0200

    Delete online_cinema (copy__for_git) directory

--- online_cinema (copy__for_git)/backend/routers/services/video_player/low.py
+++
@@ -1,7 +0,0 @@
-from backend.routers.services.video_player.base import VideoPlayer
-from backend.models import Movie
-
-
-class LowQualityPlayer(VideoPlayer):
-    def play(self, movie: Movie) -> str:
-        return f"Playing '{movie.title}' in LOW quality (360p)"
